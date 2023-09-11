20e22112d767feb1fc90171ae0d2a7c698e7cd9e
Ivan <ivanglzr01@gmail.com>
2023-09-11 15:41:40 +0200

    login, update y delete implementados en el controlador

--- controllers.js
+++ controllers.js
@@ -3,7 +3,8 @@
 const User = require("./model");
 
 const validateString = (str) => {
-  if (!str || str.trim().length === 0) return false;
+  if (!str || str.trim().length === 0 || str === null || str === undefined)
+    return false;
   return true;
 };
 
@@ -15,7 +16,7 @@ let controller = {
     });
   },
 
-  save: async (req, res) => {
+  register: async (req, res) => {
     try {
       const { name, email, password } = req.body;
 
@@ -59,6 +60,120 @@ let controller = {
         error: err,
       });
     }
+  },
+
+  login: (req, res) => {
+    let { email, password } = req.params;
+
+    if (!validateString(email) || !validateString(password)) {
+      return res.status(500).send({
+        status: "error",
+        message: "Los datos no son validos",
+      });
+    }
+
+    User.findOne({ email: email, password: password })
+      .then((user) => {
+        try {
+          if (user === null || user.length <= 0) {
+            throw "User not found";
+          }
+
+          return res.status(200).send({
+            status: "success",
+            user: user,
+          });
+        } catch (err) {
+          console.log(err);
+          return res.status(404).send({
+            status: "error",
+            message: "No se encontro el usuario",
+          });
+        }
+      })
+      .catch((err) => console.log(err));
+  },
+
+  getUser: (req, res) => {
+    let id = req.params.id;
+
+    User.findById(id)
+      .then((user) => {
+        console.log(user);
+        return res.status(200).send({
+          status: "success",
+          user: user,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        return res.status(404).send({
+          status: "error",
+          message: "No se ha encontrado el usuario",
+        });
+      });
+  },
+
+  updateUser: (req, res) => {
+    try {
+      let newUser = req.body;
+
+      if (
+        !validateString(newUser.name) ||
+        !validateString(newUser.email) ||
+        !validateString(newUser.password)
+      ) {
+        throw "Faltan datos del usuario";
+      }
+
+      let id = req.params.id;
+
+      User.findOneAndUpdate({ _id: id }, newUser, { new: true }).then(
+        (userUpdated) => {
+          console.log(userUpdated);
+
+          if (!userUpdated) {
+            throw "Error al actualizar el usuario";
+          }
+
+          return res.status(200).send({
+            status: "success",
+            user: userUpdated,
+          });
+        }
+      );
+    } catch (err) {
+      console.log(err);
+      res.status(500).send({
+        status: "error",
+        message: `${err}`,
+      });
+    }
+  },
+
+  deleteUser: (req, res) => {
+    let id = req.params.id;
+
+    User.findOneAndDelete({ _id: id })
+      .then((userRemoved) => {
+        if (!userRemoved) {
+          return res.status(404).send({
+            status: "error",
+            message: "No se ha encontrado el usuario",
+          });
+        }
+
+        return res.status(200).send({
+          status: "success",
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).send({
+          status: "error",
+          message: "Ha ocurrido un error al eliminar",
+        });
+      });
   },
 };
 
--- routes.js
+++ routes.js
@@ -7,6 +7,10 @@ let UserController = require("./controllers");
 let router = express.Router();
 
 router.get("/prueba", UserController.prueba);
-router.post("/save", UserController.save);
+router.get("/login/:email/:password", UserController.login);
+router.get("/get-user/:id", UserController.getUser);
+router.post("/register", UserController.register);
+router.put("/update-user/:id", UserController.updateUser);
+router.delete("/delete-user/:id", UserController.deleteUser);
 
 module.exports = router;
